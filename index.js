abd7edf463aef286eb1dbf2e07f6fa20a8a3f16f
vtii <>
2018-09-03 16:33:32 +0200

    UI update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// socket IO + webServer
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -20,3 +21,4 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+// - - - - - - - - - - - - - -
