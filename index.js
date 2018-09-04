0662e6de574bdf11d9cf129940d50d5cff17ce50
daluf <42932723+daluf@users.noreply.github.com>
2018-09-04 12:40:55 +0200

    Chat Core + UI Rework

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // socket IO + webServer
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-let userlist = [];
+const userlist = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,31 +14,49 @@ app.get('/style.css', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
 
   socket.on('login', function(username){
-    console.log(username);
-    
-    userlist.push({
-        "id": socket.id,
-        "user": username
-    }) // Username + ID -> Array
-    
-    console.log(userlist) // Print Array for testing purposes
-    
-    socket.emit('showChat'); // Hide Login Window and show Chat
-    socket.broadcast.emit('userConnected', username); // Send to all Clients -> New User connected
+    if (username.length <= 12) {
+      console.log(username);
+      
+      userlist.push({
+          "id": socket.id,
+          "user": username
+      }) // Username + ID -> Array
+      
+      console.log(userlist) // Print Array for testing purposes
+      
+      socket.emit('showChat'); // Hide Login Window and show Chat
+      io.emit('userConnected', username); // Send to all Clients -> New User connected
+    } else {
+      socket.emit('alertError', 'Username too long! Max. 12 Chars'); // SweetError if the Username extends 12 Chars
+    }
   });
 
   socket.on('disconnect', function(){
     console.log(socket.id + ' disconnected');
     
+    if (userlist.find(id => id.id === socket.id)) {
+      io.emit('clientDisconnect', userlist.find(id => id.id === socket.id).user); // Emit to all Clients that a User left
+    }
+
     let userIndex = userlist.indexOf(userlist.find(id => id.id === socket.id)) // Find element's index in array
-    userlist.splice(userIndex); // Remove the Item
+    userlist.splice(userIndex, 1); // Remove the Item
     
     console.log(userlist)
-    // socket.broadcast.emit('clientDisconnect', ); <- WIP
   });
+
+  socket.on('message', function(msg) {
+    io.emit('newMessage', userlist.find(id => id.id === socket.id).user, msg); // Send the new incoming Message to all Clients
+  })
+
+  socket.on('getOnlineUsers', function() { // User requests all Online Users so they can be displayed
+    let onlineList = [];
+    for (var key in userlist) {
+      onlineList.push(userlist[key].user)
+    }
+    socket.emit('getOnlineUsers', onlineList)
+  })
 
 });
 
